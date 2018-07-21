53878fa547e023d86e95b216b91240b79c6d3b4f
David Latorre <dvdn10071856@gmail.com>
2018-07-21 11:37:29 -0500

    Add Verbose to IncreaseTmpFolder.sh

--- IncreaseTmpFolder.sh
+++ IncreaseTmpFolder.sh
@@ -1,1 +1,7 @@
+echo "************************************"
+echo "Script to resize /tmp folder to 12GB"
+echo "************************************"
+
 sudo mount -o remount,size=12G /tmp
+
+echo "Folder size increased"
