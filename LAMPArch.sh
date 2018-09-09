6350b9a6799cd85e8a6952f68404f1bf823cd360
David Latorre <dvdn10071856@gmail.com>
2018-09-09 18:05:35 -0500

    Improved LAMPArch.sh script now with options to start, stop and restart

--- LAMPArch.sh
+++ LAMPArch.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 echo '*******************************************'
-echo '* Script para iniciar LAMP Server en Arch *'
+echo '*     Script para LAMP Server en Arch     *'
 echo '*******************************************'
+echo ''
+Mesg='Please enter your choice - Por favor escoja su opción: '
+echo ''
+options=("Iniciar base de datos y apache"
+"Reiniciar base de datos y apache"
+"Detener base de datos y apache"
+"Salir")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Iniciar base de datos y apache")
 echo 'iniciando Apache'
 sudo systemctl start httpd.service
 echo 'Apache iniciado'
@@ -12,3 +23,39 @@ sudo systemctl start mysqld
 echo 'MariaDB iniciado'
 echo '****************'
 echo '****************'
+;;
+
+"Reiniciar base de datos y apache")
+echo 'reiniciando Apache'
+sudo systemctl restart httpd.service
+echo 'Apache reiniciado'
+echo '****************'
+echo '****************'
+echo 'reiniciando MariaDB'
+sudo systemctl restart mysqld
+echo 'MariaDB reiniciado'
+echo '****************'
+echo '****************'
+;;
+
+
+"Detener base de datos y apache")
+echo 'Deteniendo Apache'
+sudo systemctl stop httpd.service
+echo 'Apache detenido'
+echo '****************'
+echo '****************'
+echo 'Deteniendo MariaDB'
+sudo systemctl stop mysqld
+echo 'MariaDB detenido'
+echo '****************'
+echo '****************'
+;;
+
+"Salir")
+    break
+    ;;
+*) echo invalid option;;
+    esac
+done
+
