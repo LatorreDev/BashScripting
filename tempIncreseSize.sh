f3c52b0151be11844f8d65d561916ffa495a4d2e
David Latorre <dvdn10071856@gmail.com>
2018-10-05 12:14:20 -0500

    Add bash script to increase to 12GB the size of tmp

---
+++ tempIncreseSize.sh
@@ -0,0 +1,7 @@
+echo "********************************************************"
+echo "* Script para incrementar a 12 GB la capacidad de /tmp *"
+echo "********************************************************"
+sudo mount -o remount,size=12G /tmp
+echo "Capacidad incrementada"
+echo "Script made by David Latorre"
+echo "github.com/latorredev"
