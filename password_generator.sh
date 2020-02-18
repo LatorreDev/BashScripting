fcf8bcc42656e5f9a989a5a9d364b35e67e8e7cd
David Latorre <david@latorredev.com>
2020-02-18 16:50:52 -0500

    [UPDATE] add support for OSX and add better coding practices

--- password_generator.sh
+++ password_generator.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+# Password generator
 
 genpasswd() { 
     local l=$1
         [ "$l" == ""  ] && l=16
-            tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs
+        LC_CTYPE=C tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs
         }
 
 genpasswd
