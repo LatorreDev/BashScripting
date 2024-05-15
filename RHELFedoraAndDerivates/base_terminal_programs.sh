17fa31a0ab9c7519332d58149de88cdfe7a642a0
David Latorre <david@latorredev.com>
2024-05-14 19:16:59 -0500

    FEATURE/Add script to install base terminal programs and configurantions

---
+++ RHELFedoraAndDerivates/base_terminal_programs.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+# Base programs 
+sudo dnf install kitty tmux neovim curl git zsh neofetch -y
+
+if ["$0" = "bash"]; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+echo "alias vim=nvim" >> ~/.zshrc && \
+source ~/.zshrc && \
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && \
+mkdir ~/.config/nvim && \
+touch ~/.config/nvim/init.vim && \
+git clone https://github.com/LatorreDev/MyVimConfig.git ~/vimconfig && \
+cat ~/vimconfig/vimrc > ~/.config/nvim/init.vim && \
+nvim ~/.config/nvim/init.vim
+
