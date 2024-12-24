02dc38730eaf330305e593e9f9daf2e825473d2d
David Latorre <david@latorredev.com>
2024-12-24 16:03:54 -0500

    [ADD] add fix for bethesda games sound on linux

---
+++ gaming_on_linux/bethesda_sound.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Bethesda game sound on linux
+
+# For ArchLinux and derivates
+sudo pacman -S lib32-pipewire lib32-libpulse pipewire-pulse lib32-gst-plugins-base lib32-gst-plugins-good pipewire-alsa lib32-alsa-lib lib32-alsa-pluginso
