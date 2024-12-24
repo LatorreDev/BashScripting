db9b0e2d3fd161ecc71f55687c620f33fbbc2536
David Latorre <david@latorredev.com>
2024-12-24 16:10:24 -0500

    [ADD] add multimedia programs for archlinux

---
+++ ArchRelated/multimediaArch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Multimedia programs
+
+sudo pacman -S kdenlive obs-studio audacity lmms gimp fontforge inkscape
+echo "optional, you can install the following programs through steam"
+echo "sudo pacman -S blender krita godot"
+echo  "yay -S aseprite"
