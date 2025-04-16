a776978ad79f5448a278a3651cb1d4f15f0ddb24
David Latorre <david@latorredev.com>
2025-04-15 20:59:39 -0500

    feature/add xorg for fedora

---
+++ RHELFedoraAndDerivates/xorg.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/ env bash
+# Install xorg on Fedora
+sudo dnf install gnome-session-xsession xorg-x11-drivers xorg-x11-xinit
+# You need to start your graphical session on tty mode with startx
+
